b241916e16c8ca2a7e5927431ce0b3cf5061e942
Prabhat Singh <prabhatsingh5552>
2022-12-29 15:51:15 +0530

    second commit1

---
+++ src/Test2.java
@@ -0,0 +1,11 @@
+class Test2{
+	
+    public static  void main(String [] args){
+        System.out.println("Hello");
+    
+    // adding more conditions
+    ///  adding one more commti for conflict
+    }
+
+
+}
