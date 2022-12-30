6d949574f2725990dad609d4407b3bd6676ea3a9
Prabhat Singh <prabhatsingh5552>
2022-12-30 13:59:30 +0530

    commit from 555522

--- src/Test1.java
+++ src/Test1.java
@@ -2,8 +2,9 @@
 public class Test1 {
 
 	//main method
-	
+	// 
 	public static void main(String[] args) {
 		System.out.println("Hello World Git");
+	   System.out.println("Commit from 555522");
 	}
 }
